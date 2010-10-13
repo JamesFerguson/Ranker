90904ce068509a5a9fc48bc049c372a00f613dfb
JamesFerguson <james@getup.org.au>
2010-10-13 13:22:09 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ranker::Application.config.secret_token = '159434f54a619fa99a3032c8f30d5c26d5e38d64f026229ed42faa64f0bcb5a183854e6c213394b88f40a2ecf1da1bf31071bf4ab2a08e485ef68f3edb67ebbc'
