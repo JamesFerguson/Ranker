dc862187e64fecb9c24b3253ed3318fa3746785a
JamesFerguson <james@getup.org.au>
2010-11-13 20:06:23 +0800

    Incredibly crude login system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper_method :current_user
+  
+  private
+  
+  def current_user
+    @current_user ||= session[:current_user_id] && User.find_by_id(session[:current_user_id])
+  end
 end
