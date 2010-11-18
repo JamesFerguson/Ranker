72b5d29789e0b50b38eb58a7cfd71c0b634a0925
JamesFerguson <james@getup.org.au>
2010-11-18 17:20:07 +1100

    Customised user model. Added extra devise modules. Devise auto added attr protection.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  # Include chosen devise modules. Others available are:
+  # :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :token_authenticatable, :confirmable, :lockable
+
+  # The only attributes we expect from a form, others will be protected by default.
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
