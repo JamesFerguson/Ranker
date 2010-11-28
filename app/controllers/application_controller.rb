c4b2c88a286dc46de012cff95a4164fe27bad7d3
JamesFerguson <james@getup.org.au>
2010-11-28 23:06:18 +1100

    Add devise hooks to ApplicationController to customise routing. Couldn't work out how to use devise routes (devise/sessions#new) in root and just using the current root results in a double redirect on logout that loses the flash notice.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  # Customise Devise routing
+  def after_sign_in_path_for(resource_or_scope)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    edit_reg_path = :"edit_#{scope}_registration_path"
+    send(edit_reg_path)
+  end
+  
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+  
 end
