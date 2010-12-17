17f010a2438ac32086e329f73c80c65fd1895f4c
JamesFerguson <james.ferguson7@gmail.com>
2010-12-17 11:13:15 +1100

    Implemented browser restart step with help of this gist https://gist.github.com/484787

--- features/step_definitions/app_web_steps.rb
+++ features/step_definitions/app_web_steps.rb
@@ -4,3 +4,49 @@ Then /^the page I'm on should match (.*)?$/ do |page_name|
   expected_page_source = page.source
   actual_page_source.should match(expected_page_source)
 end
+
+When /^I restart my browser$/ do
+  browser = BrowserFactory.find_browser
+  browser.restart
+end
+
+# Trying it this way instead of monkey patching - revisit
+class BrowserFactory
+  def self.find_browser
+    driver = Capybara.current_session.driver
+    driver_klass_name = driver.class.name.sub!(driver.class.parent_name + '::', '')
+    browser = (driver_klass_name + "Browser").constantize.new(driver)
+    return browser
+  rescue NameError => e
+    puts "Getting browser cookies not implemented for #{driver.class.name}"
+    raise
+  end
+end
+
+class RackTestBrowser
+  def initialize(driver)
+    @driver = driver
+  end
+  
+  def restart
+    self.cookies.reject! do |cookie|
+      cookie.expired? != false
+    end
+  end
+  
+  def cookies
+    @cookies ||= self.cookie_jar.instance_variable_get(:@cookies)
+  end
+  
+  def cookie_jar
+    @cookie_jar ||= self.browser.cookie_jar
+  end
+  
+  def browser
+    @browser ||= self.session.instance_variable_get(:@rack_mock_session)
+  end
+  
+  def session
+    @session ||= @driver.current_session
+  end
+end
